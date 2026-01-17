2dd780a645a5ab43c6c5b759d0e5433ba5390ec8
Reach420 <justreach555@gmail.com>
2026-01-18 02:24:50 +1000

    implement checkResources to see if there are enough resources when ordering

--- main.py
+++ main.py
@@ -29,18 +29,35 @@ MENU = {
 resources = {
     "water": 300,
     "milk": 200,
-    "coffee": 100,
+    "coffee": 10,
     "money": 0
 }
-off = False
-while not off:
-    coffee = input("​What would you like? (espresso/latte/cappuccino)")
 
-# All time commands
+def checkResources(coffee):
+    efficient = True
+    for ingredient in MENU[coffee]['ingredients'].keys():
+        if resources[ingredient] < MENU[coffee]['ingredients'][ingredient]:
+            efficient = False
+            return efficient, ingredient
+    return efficient, ingredient
+
+# ----- Main Loop -----
+off = False
+efficient = True
+while not off:
+    coffee = input("​What would you like? (espresso/latte/cappuccino): ")
+
+    if coffee == "espresso":
+        efficient, ingredient = checkResources('espresso')
+
+    if not efficient:
+        print(f"Sorry, There is not enough {ingredient}")
+
+# ----- All time commands ------
     if coffee == "off":
         sys.exit()
     if coffee == "report":
         print(f"Water: {resources['water']}ml")
-        print(f"Water: {resources['water']}ml")
-        print(f"Water: {resources['water']}ml")
-        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: {resources['money']}$")

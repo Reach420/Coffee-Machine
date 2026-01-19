1bc964055a00c47e6b654ea7d496091d08740b73
Reach420 <justreach555@gmail.com>
2026-01-19 21:33:17 +1000

    all time commands are now in getInput to always read commands, created a processOrder function to process different coffee(Unfinished)

--- main.py
+++ main.py
@@ -29,44 +29,57 @@ MENU = {
 resources = {
     "water": 300,
     "milk": 200,
-    "coffee": 10,
+    "coffee": 100,
     "money": 0
 }
+def getInput(prompt):
+    command = True
+    while command:
+        Finput = input(prompt)
+        if Finput == "off":
+            sys.exit()
+        if Finput == "report":
+            print(f"Water: {resources['water']}ml")
+            print(f"Milk: {resources['milk']}ml")
+            print(f"Coffee: {resources['coffee']}g")
+            print(f"Money: {resources['money']}$")
+        else:
+            command = False
+    return Finput
+
 
 def checkResources(coffee):
-    efficient = None
+    efficient = True
     for ingredient in MENU[coffee]['ingredients'].keys():
         if resources[ingredient] < MENU[coffee]['ingredients'][ingredient]: # Compares resources with the required ingredients
             efficient = False
             return efficient, ingredient
     return efficient, ingredient
 
-# ----- Main Loop -----
-off = False
-while not off:
-    coffee = input("​What would you like? (espresso/latte/cappuccino): ")
+def processOrder(resources,coffee):
+    efficient, ingredient = checkResources('espresso')
 
-    efficient = None
-    if coffee == "espresso":
-        efficient, ingredient = checkResources('espresso')
-
-    if efficient:
-        print("Please insert the coins")
-        quarters = input("How many quarters? ")
-        dimes = input("How many dimes? ")
-        nickles = input("How many nickles? ")
-        pennies = input("How many pennies? ")
-        # # Deduct resources values
-        # for ingredient in MENU[coffee]['ingredients'].keys():
-        #     resources[ingredient] -= MENU[coffee]['ingredients'][ingredient]
-    elif not efficient:
+    if not efficient:
         print(f"Sorry, There is not enough {ingredient}")
+        return
 
-# ----- All time commands ------
-    if coffee == "off":
-        sys.exit()
-    if coffee == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: {resources['money']}$")
+    print("Please insert the coins")
+    quarters = float(getInput("How many quarters? ")) * 0.25
+    dimes = float(getInput("How many dimes? ")) * 0.10
+    nickles = float(getInput("How many nickles? ")) * 0.05 
+    pennies = float(getInput("How many pennies? ")) * 0.01
+    resources["money"] = quarters + dimes + nickles + pennies
+    if resources["money"] < MENU[coffee]["cost"]:
+        print("Sorry that's not enough money. Money refunded")
+    
+    # Deduct resources values
+    for ingredient in MENU[coffee]['ingredients'].keys():
+        resources[ingredient] -= MENU[coffee]['ingredients'][ingredient]
+
+
+# ----- Main Loop -----
+while True:
+    coffee = getInput("​What would you like? (espresso/latte/cappuccino): ")
+    efficient = True
+    if coffee == "espresso":
+        processOrder(resources,"espresso")
